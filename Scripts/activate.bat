a0bb93f46055e2f26e2060244126b475e65ec6cf
MarcosP-Costa <marcos_nokbuk@hotmail.com>
2023-12-22 00:41:13 -0300

    Att 22-12

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Marco\OneDrive\Documentos\Github\Projeto_Batocera.Linux
+set VIRTUAL_ENV=C:\Users\Marcos Paulo\Documents\Github\Projeto_Batocera.Linux
 
 if not defined PROMPT set PROMPT=$P$G
 
